925e5d7d5b1844d17aa7330608e0c65925f98fc4
C0smar <116185036+C0smar@users.noreply.github.com>
2024-09-29 11:50:43 -0300

    Para iniciar primero usar en consola npm install

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavComponent from './components/navBar/NavComponent'
+import Home from './components/home/Home'
+import Footer from './components/footer/Footer'
+
+
+function App() {
+  return (
+    <>
+      <NavComponent/>
+      <Home/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
